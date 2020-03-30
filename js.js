67f66b81f0f4f8bbb3c77c9a5dff2db32d309eda
akhilesh <appumithr@gmail.com>
2020-03-30 17:37:25 +0530

    first commit

---
+++ js.js
@@ -0,0 +1,94 @@
+
+/*var name = prompt('what is your name ?');
+var age = prompt('what is your age ?');
+//age >= 18 ? console.log(name + " drinks beer") : console.log(name + " drinks juice");
+
+var drink = age >= 18 ? "beer" : "juice";
+console.log(name + " drinks " +drink )
+ var job = prompt('what is your job  ?');
+/*
+ switch(job){
+   case "teacher":
+     console.log(name+" teaches");
+     break;
+    case "painter":
+      console.log(name+"is a painter");
+      break;
+      default:
+       console.log(name+" does something else");
+
+ }
+*/
+/*
+ switch(true){
+   case age<=13:
+     console.log(name+" teaches");
+     break;
+    case age>=20 :
+      console.log(name+"is a painter");
+      break;
+      default:
+       console.log(name+" does something else");
+
+ }
+*/
+/*<<<<<functions>>>>>>
+
+function Yage(year){
+  return 2020 - year;
+}
+var age = prompt("Year of Birth ?");
+console.log(Yage(age));
+
+var blah = Yage(age);
+function retirement(B){
+  var yr= 65-blah;
+  console.log(yr +"more years");
+}
+retirement(age)
+*/
+/*///////arrays  date : march 27 2020
+
+var array = ['piece','of','shit'];
+var a = new Array(19, 90, 09, 89);
+console.log(array);
+a[a.length]=9;
+a.pop();
+a.push(100);
+a.unshift(9);
+
+console.log(a);
+*/
+/*
+var object = {
+FirstName:'Akhilesh',
+LastName: 'PM',
+Status :"Student",
+MaritalStatus:'Unmarried',
+};
+console.log(object['MaritalStatus']);
+console.log(object);
+
+var pupil = {
+  name:'Appu',
+  class:9,
+  rollNo:4,
+  parentName:'Mithran',
+  place:'Peringottukara'
+};
+
+console.log(pupil);
+
+*/
+/*
+var emply= {
+  Fname:'john',
+  Lname:'Cena',
+  Family:['Lisa','hume','Mortex'],
+  CalAge: function() {
+    this.age = 2020 - this.Birthyear;
+  };
+emply.Birthyear = prompt('what is your age ?');
+emply.CalAge();
+console.log(emply);
+*/
